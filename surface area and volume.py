aa69e3756025b8bd4121079639b966a603ab99e7
Sathiya535 <130639868+Sathiya535@users.noreply.github.com>
2023-05-18 12:26:05 +0530

    Add files via upload

---
+++ surface area and volume.py
@@ -0,0 +1,8 @@
+pi=3.14
+radian=int(input("enter the raian of the cylinder:"))
+height=int(input("enter the height of the cylinder:"))
+surface_area=2*pi*radian
+volume=pi*radian*radian*height
+print("the volume  the cylinder is:",volume)
+print("the surface area of the cylinder:",surface_area)
+
