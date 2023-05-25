ee751833dbddcdbec720b947fdb03f108c18394b
Sathiya535 <130639868+Sathiya535@users.noreply.github.com>
2023-05-25 11:37:52 +0530

    Add files via upload

---
+++ power of nth num.py
@@ -0,0 +1,7 @@
+num=int(input("enter the number:"))
+power=int(input("enter the power:"))
+if(num%2==0):
+   print("the n th power of a number is even")
+else:
+     print("the n th power of a number is odd")
+       
