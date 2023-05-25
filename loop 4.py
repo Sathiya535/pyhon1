3683d33f7a7f07e5f14f9453256f06d3e1050e1d
Sathiya535 <130639868+Sathiya535@users.noreply.github.com>
2023-05-25 11:56:11 +0530

    Add files via upload

---
+++ loop 4.py
@@ -0,0 +1,6 @@
+'''Write a program to display the multiplication table as below 
+Sample Input 5'''
+table=int(input("enter the multiplication table"))
+for i in range(1,11):
+    print(i,"*",table,"=",i*table)
+
