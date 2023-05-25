b9fe17297d45c9681d6d39268495a1b084b19975
Sathiya535 <130639868+Sathiya535@users.noreply.github.com>
2023-05-25 11:24:22 +0530

    Add files via upload

---
+++ p or ne 2.py
@@ -0,0 +1,8 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num=int(input("enter the number:"))
+enter the number:
+================ RESTART: C:/Users/STUDENT/ps or ne pyton.py ================
+enter the number:55
+positive
+>>> 
