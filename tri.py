87a56bbce1f1cb58c3ab6fcdf5b55bfee633f6de
Sathiya535 <130639868+Sathiya535@users.noreply.github.com>
2023-05-25 11:30:45 +0530

    Add files via upload

---
+++ tri.py
@@ -0,0 +1,7 @@
+x=int(input("enter the number 1;"))
+y=int(input("enter the number 2:"))
+z=int(input("enter the numer 3:"))
+if(x+y>z and x+z>y and y+z>x):
+    print("possible")
+else:
+    print("not possible")
