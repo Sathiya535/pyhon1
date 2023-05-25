cd6d180a3f811f702a948d386f5d319973f926f5
Sathiya535 <130639868+Sathiya535@users.noreply.github.com>
2023-05-25 11:15:17 +0530

    Add files via upload

---
+++ positive or negative.py
@@ -0,0 +1,8 @@
+##positive or negative
+num=int(input("enter the number:"))
+if(num>0):
+    print("positive")
+    elif(num==0):
+        print("zero")
+        else(num<0):
+            print("positive")
